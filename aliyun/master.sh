fcf2f220f951cd82c4a75f47615c98a7bd340895
Haicheng Jiang <javac2005@sina.com>
2019-02-07 19:46:39 +0800

    add files

---
+++ aliyun/master.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+export HOST0=172.17.220.38
+export NAME0=master0
+
+export HOST1=172.17.220.39
+export NAME1=master1
+
+export HOST2=172.17.220.40
+export NAME2=master2
+
+export LOAD_BALANCER_DNS=apiserver.hc68.cn
+
+HOSTS=(${HOST0} ${HOST1} ${HOST2})
+NAMES=(${NAME0} ${NAME1} ${NAME2})
+
+rm -rf /etc/kubernetes
+
+#配置本机hosts
+for i in "${!HOSTS[@]}"; do
+	echo "${HOSTS[$i]}  ${NAMES[$i]}" >> /etc/hosts
+done
+
+#生成密钥，做免密登录
+ssh-keygen -t rsa
+for i in "${!HOSTS[@]}"; do
+	ssh-copy-id ${NAMES[$i]}
+done
+
+#配置hosts
+for i in "${!HOSTS[@]}"; do
+	ssh ${NAMES[$i]} "hostnamectl set-hostname ${NAMES[$i]}"
+	ssh ${NAMES[$i]} "echo ${HOSTS[$j]}  ${LOAD_BALANCER_DNS} >> /etc/hosts"
+	if [[ ${NAMES[$i]} != ${NAMES[0]} ]]
+		then
+			for j in "${!HOSTS[@]}"; do
+				ssh ${NAMES[$i]} "echo ${HOSTS[$j]}  ${NAMES[$j]} >> /etc/hosts"
+			done
+	fi
+done
+
+#安装必要软件
+for i in "${!HOSTS[@]}"; do
+	ssh ${NAMES[$i]} "yum install -y epel-release  net-tools wget vim \
+		ntpdate bash-completion lrzsz unzip bridge-utils.x86_64"
+done
+
+#初始化前准备
+chmod +x pre.sh
+for i in "${!HOSTS[@]}"; do
+	scp pre.sh ${NAMES[$i]}:
+	ssh ${NAMES[$i]} ./pre.sh
+done
+
+#下载镜像
+for imageName in \
+	kube-proxy:v1.13.1 \
+	kube-apiserver:v1.13.1 \
+	kube-controller-manager:v1.13.1 \
+	kube-scheduler:v1.13.1 \
+	coredns:1.2.6 \
+	etcd:3.2.24 \
+	pause:3.1; \
+do \
+	docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/${imageName}; \
+	docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/${imageName} k8s.gcr.io/${imageName}; \
+	docker rmi -f registry.cn-hangzhou.aliyuncs.com/google_containers/${imageName}; \
+done
+
+docker pull registry.cn-beijing.aliyuncs.com/common-registry/flannel:v0.10.0-amd64
+docker tag registry.cn-beijing.aliyuncs.com/common-registry/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
+docker rmi -f registry.cn-beijing.aliyuncs.com/common-registry/flannel:v0.10.0-amd64
+
+docker pull registry.cn-beijing.aliyuncs.com/common-registry/nginx-ingress-controller:0.21.0
+docker tag registry.cn-beijing.aliyuncs.com/common-registry/nginx-ingress-controller:0.21.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.21.0
+docker rmi -f registry.cn-beijing.aliyuncs.com/common-registry/nginx-ingress-controller:0.21.0
+
+mkdir -p images
+for imageName in $(docker images | awk 'NR!=1{print $1}') ; do
+	docker save $imageName  -o "images/${imageName##*/}.tar";
+done
+
+for i in "${!HOSTS[@]}"; do
+	if [[ ${NAMES[$i]} != ${NAMES[0]} ]]
+		then
+			scp -r images ${NAMES[$i]}:
+			ssh ${NAMES[$i]} 'cd images && for imageName in $(ls); do  docker load < $imageName; done'
+	fi
+done
+
+#初始化配置文件：kubeadm-config.yaml
+cat << EOF > kubeadm-config.yaml
+apiVersion: kubeadm.k8s.io/v1beta1
+kind: ClusterConfiguration
+kubernetesVersion: v1.13.1
+apiServer:
+  certSANs:
+  - ${LOAD_BALANCER_DNS}
+  - ${HOST0}
+  - ${HOST1}
+  - ${HOST2}
+  - ${NAME0}
+  - ${NAME1}
+  - ${NAME2}
+networking:
+  podSubnet: 10.244.0.0/16
+  serviceSubnet: 10.96.0.0/12
+controlPlaneEndpoint: ${LOAD_BALANCER_DNS}:6443
+EOF
+
+#初始化master
+kubeadm init --config=kubeadm-config.yaml
+mkdir -p .kube && rm -rf .kube/config && cp /etc/kubernetes/admin.conf .kube/config
+sysctl net.bridge.bridge-nf-call-iptables=1
+kubectl apply -f kube-flannel.yml
+
+echo '60秒后开始初始化其他master节点'
+sleep 60
+
+#加入其它master - 拷贝证书
+for i in "${!NAMES[@]}"; do
+	if [[ ${NAMES[$i]} != ${NAMES[0]} ]]
+		then
+			ssh ${NAMES[$i]} "mkdir -p /etc/kubernetes/pki/etcd"
+			scp /etc/kubernetes/pki/ca.crt ${NAMES[$i]}:/etc/kubernetes/pki/
+			scp /etc/kubernetes/pki/ca.key ${NAMES[$i]}:/etc/kubernetes/pki/
+			scp /etc/kubernetes/pki/sa.key ${NAMES[$i]}:/etc/kubernetes/pki/
+			scp /etc/kubernetes/pki/sa.pub ${NAMES[$i]}:/etc/kubernetes/pki/
+			scp /etc/kubernetes/pki/front-proxy-ca.crt ${NAMES[$i]}:/etc/kubernetes/pki/
+			scp /etc/kubernetes/pki/front-proxy-ca.key ${NAMES[$i]}:/etc/kubernetes/pki/
+			scp /etc/kubernetes/pki/etcd/ca.crt ${NAMES[$i]}:/etc/kubernetes/pki/etcd/
+			scp /etc/kubernetes/pki/etcd/ca.key ${NAMES[$i]}:/etc/kubernetes/pki/etcd/
+			scp /etc/kubernetes/admin.conf ${NAMES[$i]}:/etc/kubernetes/
+	fi
+done
+
+#加入其它master - 准备token
+MASTER_TOKEN=`kubeadm token list | awk 'NR==2{print $1}'`
+DISCOVERY_TOKEN=`openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | \
+openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'`
+
+#加入其它master - 初始化、加入
+for i in "${!NAMES[@]}"; do
+	if [[ ${NAMES[$i]} != ${NAMES[0]} ]]
+		then
+			ssh ${NAMES[$i]} "kubeadm join ${LOAD_BALANCER_DNS}:6443 \
+			--token ${MASTER_TOKEN} --discovery-token-ca-cert-hash \
+			sha256:${DISCOVERY_TOKEN} --experimental-control-plane"
+			sleep 5
+	fi
+done
---
+++ aliyun/pre.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+echo '############停止并关闭防火墙'
+systemctl stop firewalld.service || true
+systemctl disable firewalld.service || true
+
+echo '############禁用SELINUX'
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+echo '############设置转发规则'
+modprobe bridge
+modprobe br_netfilter
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+vm.swappiness=0
+EOF
+
+sysctl --system
+
+echo '############禁用虚拟内存'
+swapoff -a
+sed -i 's/^.*swap*/#&/g' /etc/fstab
+
+echo '############同步时间'
+rm -rf /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ntpdate -u cn.pool.ntp.org
+
+echo '############设置阿里云源'
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+echo '############安装并启动docker的17.03.2.ce版'
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast
+sudo yum install -y --setopt=obsoletes=0 \
+	docker-ce-selinux-17.03.2.ce-1.el7.centos \
+	docker-ce-17.03.2.ce-1.el7.centos
+systemctl enable docker.service
+systemctl start docker.service
+
+echo '############安装kubeadm、kubectl、kubelet，1.13.1版本'
+yum install -y kubelet-1.13.1 \
+	kubeadm-1.13.1 \ 
+	kubectl-1.13.1  --disableexcludes=kubernetes
+systemctl daemon-reload
+systemctl enable kubelet
+systemctl start kubelet
+
+#有时候flannel的InitContainer无法正常运行，手动设置：
+echo '############设置CNI网络'
+mkdir -p /etc/cni/net.d/
+cat <<EOF> /etc/cni/net.d/10-flannel.conflist
+{
+  "name": "cbr0",
+  "plugins": [
+    {
+      "type": "flannel",
+      "delegate": {
+        "hairpinMode": true,
+        "isDefaultGateway": true
+      }
+    },
+    {
+      "type": "portmap",
+      "capabilities": {
+        "portMappings": true
+      }
+    }
+  ]
+}
+EOF
+
+#echo '############命令补全'
+#source /usr/share/bash-completion/bash_completion
+#source <(kubectl completion bash)
+#echo "source <(kubectl completion bash)" >> ~/.bashrc
